df5f9d11f6efbda0397930c7f5be70c1ffec3edb
Kendall Jones <kendalljones@Kendalls-MacBook-Pro.local>
2020-02-07 12:55:29 -0500

    Changes to layout and stubbing in mailing code

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,46 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
 var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
     res.render('contact', { title: 'Contact' });
-  });
+});
+
+router.post('/', function(req, res, next) {
+    // var Kitten = req.Kitten;
+    console.log(req.body); // PROBLEM: it returns empty, while I expect req.body
+
+    var transporter = nodemailer.createTransport({
+      host: 'smtp.mail.yahoo.com',
+            port: 587,
+            service:'yahoo',
+            secure: false,
+            auth: {
+               user: 'kendalljones99@yahoo.com',
+               pass: 'pukvnuzghmjppyse'
+            },
+            debug: false,
+            logger: true 
+    });
+    
+    var mailOptions = {
+      from: 'kendalljones99@yahoo.com',
+      to: 'kenmail99@yahoo.com',
+      subject: 'Sending Email using Node.js',
+      text: 'That was easy!'
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+
+    //res.redirect('/meow_response');
+    next();
+});
 
 module.exports = router;
